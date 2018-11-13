4509936fe368611dd3857aad385feb558db87c7b
duong huynh bao <duonghuynhbaocr@gmail.com>
2018-11-13 18:01:17 +0700

    add more field in result csv 400000

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function getblockbyheight(block_height, promise_progress_cb = {}) {
                 promise_progress_cb(); //use
             }
             if (err) {
-               // console.log(err);
+                // console.log(err);
                 reject(err);
             } else {
                 resolve(body);
@@ -35,52 +35,63 @@ var progress_cb = function () {
 }
 
 function parse_block(block_obj) {
-    // fs.appendFileSync(fname,"height,nTime,nonce,nDifficulty,average_hash_calcualtions,targetHash");
+    // fs.appendFileSync(fname,"height,fee,numberTXs,nTime,date_str,nonce,nbits,nDifficulty,average_hash_calcualtions,blockhash,targetHash");
     let nbit = block_obj.bits;
     let bnTarget = btcDiff.nbits_to_bnTarget(nbit);
     let nDiff = btcDiff.calculate_nDifficulty(bnTarget);
     let number_calcs = btcDiff.average_calculation_to_get_bnTarget(bnTarget);
     let targetStr = btcDiff.convert_bnTarget256Str(bnTarget);
+    let utc_stamp = block_obj.time * 1000;
+    let theDate = new Date(utc_stamp);
+    let dateString = theDate.toGMTString().replace(",", "");
+
+    let fee = block_obj.fee;
+    let number_of_tx = block_obj.n_tx;
 
     let info = '';
     info = info + block_obj.height + ',';
+    info = info + fee + ',';
+    info = info + number_of_tx + ',';
     info = info + block_obj.time + ',';
+    info = info + dateString + ',';
     info = info + block_obj.nonce + ',';
+    info = info + nbit + ',';
     info = info + nDiff + ',';
     info = info + number_calcs + ',';
+    info = info + block_obj.hash + ',';
     info = info + targetStr;
     return info;
 }
-function InvestigateBTC(start_height, end_height) {
-    // let start_height=0;
-    // let end_height=10;
-    let fname = 'investigate_' + start_height + '_' + end_height + '.csv';
-    if (fs.existsSync(fname)) {
-        fs.unlinkSync(fname);
-    }
+// function InvestigateBTC(start_height, end_height) {
+//     // let start_height=0;
+//     // let end_height=10;
+//     let fname = 'investigate_' + start_height + '_' + end_height + '.csv';
+//     if (fs.existsSync(fname)) {
+//         fs.unlinkSync(fname);
+//     }
 
-    let headline = "height,nTime,nonce,nDifficulty,average_hash_calcualtions,targetHash" + "\n";
-    fs.appendFileSync(fname, headline);
+//     let headline = "height,nTime,nonce,nDifficulty,average_hash_calcualtions,targetHash" + "\n";
+//     fs.appendFileSync(fname, headline);
 
-    var promiseArr = [];
-    progress_cnt = end_height - start_height;
-    for (let i = start_height; i < end_height; i++) {
-        let pm = getblockbyheight(i, progress_cb);
-        promiseArr.push(pm);
-    }
-    Promise.all(promiseArr).then(function (blocks) {
-        let nres = blocks.length;
-        for (let i = 0; i < nres; i++) {
-            let obj_block = blocks[i].blocks[0];
-            // console.log(obj_block.height);
-            // fs.appendFileSync(fname,"height,nTime,nonce,nDifficulty,average_hash_calcualtions");
-            let inf = parse_block(obj_block);
-            fs.appendFileSync(fname, inf + "\n");
-        }
+//     var promiseArr = [];
+//     progress_cnt = end_height - start_height;
+//     for (let i = start_height; i < end_height; i++) {
+//         let pm = getblockbyheight(i, progress_cb);
+//         promiseArr.push(pm);
+//     }
+//     Promise.all(promiseArr).then(function (blocks) {
+//         let nres = blocks.length;
+//         for (let i = 0; i < nres; i++) {
+//             let obj_block = blocks[i].blocks[0];
+//             // console.log(obj_block.height);
+//             // fs.appendFileSync(fname,"height,nTime,nonce,nDifficulty,average_hash_calcualtions");
+//             let inf = parse_block(obj_block);
+//             fs.appendFileSync(fname, inf + "\n");
+//         }
 
-    }).catch(console.error);
+//     }).catch(console.error);
 
-}
+// }
 function promiseTimeout(time) {
     return new Promise(function (resolve, reject) {
         setTimeout(function () { resolve(time); }, time);
@@ -97,7 +108,10 @@ async function InvestigateBtcAsync(start_height, end_height, step_jump = 1) {
     let init_cnt = end_height - start_height;
     init_progres_cb(init_cnt, step_jump);
 
-    let headline = "height,nTime,nonce,nDifficulty,average_hash_calcualtions,targetHash" + "\n";
+    // let headline = "height,nTime,nonce,nDifficulty,average_hash_calcualtions,targetHash" + "\n";
+    // let headline="height,nTime,date_str,nonce,nbits,nDifficulty,average_hash_calcualtions,blockhash,targetHash"+"\n";
+    let headline = "height,fee,numberTXs,nTime,date_str,nonce,nbits,nDifficulty,average_hash_calcualtions,blockhash,targetHash" + "\n";
+
     fs.appendFileSync(fname, headline);
 
     for (let i = start_height; i < end_height; i += step_jump) {
@@ -114,5 +128,5 @@ async function InvestigateBtcAsync(start_height, end_height, step_jump = 1) {
 
     }
 }
-InvestigateBtcAsync(0, 200000, 200);
+InvestigateBtcAsync(0, 400000, 250);
 //InvestigateBTC(0,5000);
